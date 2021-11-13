9611fb0e6eb78a2799a0be83005603754af0a77e
Alexey Borisenko <frozenpriest@mail.ru>
2021-11-13 23:31:09 +0300

    unused import lint fix

--- app/src/main/java/com/frozenpriest/ui/MainActivity.kt
+++ app/src/main/java/com/frozenpriest/ui/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.frozenpriest.ui
 
 import android.os.Bundle
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.fragment.app.FragmentActivity
 import com.frozenpriest.R
-import com.frozenpriest.theme.TemplateTheme
 import com.frozenpriest.ui.common.BaseActivity
 
 class MainActivity : BaseActivity() {
